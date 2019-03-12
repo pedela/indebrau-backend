39caf91bdc4005bf1bc99a5481c502b624284c2f
Peter de Lange <lange@dbis.rwth-aachen.de>
2019-03-12 19:12:24 +0100

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,10 @@ module.exports = {
             'error',
             2
         ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
+//        'linebreak-style': [
+//            'error',
+//            'unix'
+//        ],
         'quotes': [
             'error',
             'single'

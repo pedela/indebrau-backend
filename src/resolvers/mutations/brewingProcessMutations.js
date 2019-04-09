63045b7019b7e1ee842751fe60e63ca9a278c265
Peter de Lange <lange@dbis.rwth-aachen.de>
2019-04-09 20:56:10 +0200

    added active step progress mutation

--- src/resolvers/mutations/brewingProcessMutations.js
+++ src/resolvers/mutations/brewingProcessMutations.js
@@ -44,11 +44,53 @@ const brewingProcessMutations = {
     }
     return createdBrewingProcess;
   },
+  async advanceBrewingProcess(parent, args, ctx, info) {
+    checkUserPermissions(ctx, ['ADMIN']);
+    const where = { id: args.id };
+    let { activeSteps } = await ctx.db.query.brewingProcess(
+      { where: { id: args.id } },
+      '{ activeSteps }'
+    );
+    let newActiveSteps = args.newActiveSteps;
+    let finishedSteps = args.finishedSteps;
+    // I guess here has to be a lot of logic (and additional arguments passed in the mutation)
+    // for now, let's settle by updating the steps alone
+    if (!newActiveSteps) {
+      newActiveSteps = [];
+    }
+    if (!finishedSteps) {
+      finishedSteps = [];
+    }
+    for (let i = 0; i < finishedSteps.length; i++) {
+      if (!activeSteps.includes(finishedSteps[i])) {
+        throw new Error('Step was not active ' + finishedSteps[i]);
+      }
+      if (newActiveSteps.includes(finishedSteps[i])) {
+        throw new Error('Cannot remove and add same step ' + finishedSteps[i]);
+      }
+      activeSteps.splice(i, 1);
+    }
+    for (let i = 0; i < newActiveSteps.length; i++) {
+      if (activeSteps.includes(newActiveSteps[i])) {
+        throw new Error('Step was already active ' + newActiveSteps[i]);
+      }
+      activeSteps.push(newActiveSteps[i]);
+    }
+    console.log(activeSteps);
+    const data = { activeSteps: { set: activeSteps } };
+    return await ctx.db.mutation.updateBrewingProcess(
+      { where, data },
+      info
+    );
+  },
 
   async deleteBrewingProcess(parent, args, ctx, info) {
     checkUserPermissions(ctx, ['ADMIN']);
     const where = { id: args.id };
-    const deletedBrewingProcess = await ctx.db.mutation.deleteBrewingProcess({ where }, info);
+    const deletedBrewingProcess = await ctx.db.mutation.deleteBrewingProcess(
+      { where },
+      info
+    );
     // update cache (associated graphs might be deleted cascadingly)
     await activeGraphCache(ctx, true);
     if (!deletedBrewingProcess) {

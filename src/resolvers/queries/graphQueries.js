7dc97375ae5284e362d026c1091f801c8727f6ee
Peter de Lange <lange@dbis.rwth-aachen.de>
2020-10-15 14:52:09 +0200

    further bugfixing

--- src/resolvers/queries/graphQueries.js
+++ src/resolvers/queries/graphQueries.js
@@ -3,32 +3,35 @@ const { reduceGraphDataEvenly } = require('../../utils/reduceGraphDataEvenly');
 const { cachedSensorData } = require('../../utils/caches');
 
 const graphQueries = {
-  async graphs(parent, { data_points, active }, ctx, info) {
+  async graphs(parent, { data_points, active }, ctx) {
     checkUserPermissions(ctx, ['ADMIN']);
-
     let activeGraphs = null;
     if (active) {
       activeGraphs = { active: active };
     }
-    const graphs = await ctx.prisma.graph.findMany(
-      {
-        where: { ...activeGraphs }
-      },
-      info
-    );
-
-    // reduce returned graph data evenly across time
+    const graphs = await ctx.prisma.graph.findMany({
+      where: { ...activeGraphs },
+      include: {
+        GraphData: {}
+      }
+    });
+    // reduce returned graph data evenly over time
     graphs.map((graph) => {
-      graph.graphData = reduceGraphDataEvenly(graph.graphData, data_points);
+      graph.GraphData = reduceGraphDataEvenly(graph.GraphData, data_points);
     });
     return graphs;
   },
 
-  async graph(parent, { id, data_points }, ctx, info) {
+  async graph(parent, { id, data_points }, ctx) {
     checkUserPermissions(ctx, ['USER'], undefined, id);
-    const graph = await ctx.prisma.graph.findOne({ where: { id: id } }, info);
-    // reduce returned graph data evenly across time
-    graph.graphData = reduceGraphDataEvenly(graph.graphData, data_points);
+    const graph = await ctx.prisma.graph.findOne({
+      where: { id: id },
+      include: {
+        GraphData: {}
+      }
+    });
+    // reduce returned graph data evenly over time
+    graph.GraphData = reduceGraphDataEvenly(graph.GraphData, data_points);
     return graph;
   },
 
